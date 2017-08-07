e25bd8463972f6c4991bc21c9d6a7845d41f2e50
Tomasz Sikora <frakti7@gmail.com>
2017-08-08 01:08:29 +0200

    Make time label short

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
     <pre v-for="log in logs">
-      {{log.at}} {{ log.message }}
+      {{log.at | time}} {{ log.message }}
     </pre>
   </div>
 </template>
 
 <script>
 import logs from '../../example-logs.json'
+import moment from 'moment'
 
 export default {
   name: 'app',
   data () {
     return {
       logs
+    }
+  },
+  filters: {
+    time: (isoDate) => {
+      // return "a";
+      return moment.utc(isoDate).format('HH:mm:ss.SSS')
     }
   }
 }
