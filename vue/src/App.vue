cb0929537a9426636ab6bd878fd732700b55d95f
Tomasz Sikora <frakti7@gmail.com>
2017-08-08 01:26:26 +0200

    Show indication of beggining of the day

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <pre v-for="log in logs">
+    <pre v-for="(log, idx) in logs">
+      <div v-if="isNewDay(idx)">{{log.at | date}}</div>
       {{log.at | time}} {{ log.message }}
     </pre>
   </div>
@@ -17,11 +18,21 @@ export default {
       logs
     }
   },
-  filters: {
-    time: (isoDate) => {
-      // return "a";
-      return moment.utc(isoDate).format('HH:mm:ss.SSS')
+  methods: {
+    isNewDay: function (index) {
+      if (index === 0) {
+        return true;
+      }
+
+      const prevLog = this.logs[index - 1]
+      const currentLog = this.logs[index]
+      // return false
+      return !moment(prevLog.at).isSame(currentLog.at, 'day');
     }
+  },
+  filters: {
+    time: (isoDate) => moment.utc(isoDate).format('HH:mm:ss.SSS'),
+    date: (isoDate) => moment.utc(isoDate).format('DDD MMMM YYYY')
   }
 }
 </script>
